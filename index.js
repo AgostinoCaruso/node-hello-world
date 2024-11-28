ac2f5750d2773382fbb60977426755e8651bd7a0
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-28 17:36:51 +0100

    add nullish coalescing operator

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-console.log("Hello World");
+//console.log("Hello World");
 //node index.js
 //npm start
 
-console.log("Hello Boolean");
+//console.log("Hello Boolean");
 //npm run dev
 
 //taglio via i primi 2 indici che sono il path
-const parolaPassata = process.argv.slice(2);
-console.log(`Hello ${parolaPassata}`);
+let parolaPassata = process.argv.slice(2);
+console.log(`Hello ${parolaPassata.length > 0 ? parolaPassata : "default"}`);
+
+const parolaPassata2 = process.argv[2];
+console.log(`Hello ${parolaPassata2 ?? "default"}`);
 // node index.js "parola passata"
