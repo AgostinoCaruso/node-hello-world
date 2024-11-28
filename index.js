07b6a9fc858946bf547edc48c52c129d04091b0c
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-28 14:06:17 +0100

    print processArgv argument

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ console.log("Hello World");
 //node index.js
 //npm start
 
-console.log("Prova watch");
+console.log("Hello Boolean");
 //npm run dev
+
+//taglio via i primi 2 indici che sono il path
+const boh = process.argv.slice(2);
+console.log(`Hello ${boh}`);
+// node index.js "parola passata"
