a2e57b4ef63b5c382edb968a89d53041abc4db51
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-28 18:11:27 +0100

    fix with .join

--- index.js
+++ index.js
@@ -7,8 +7,8 @@
 
 //taglio via i primi 2 indici che sono il path
 let parolaPassata = process.argv.slice(2);
-console.log(`Hello ${parolaPassata.length > 0 ? parolaPassata : "default"}`);
+console.log(`Hello ${parolaPassata.length > 0 ? parolaPassata.join(" ") : "default"}`);
 
 const parolaPassata2 = process.argv[2];
-console.log(`Hello ${parolaPassata2 ?? "default"}`);
+//console.log(`Hello ${parolaPassata2 ?? "default"}`);
 // node index.js "parola passata"
