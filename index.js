04138294e89f840cc7756450a1eb9834c7cd81ad
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-28 15:16:24 +0100

    fix better naming const

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ console.log("Hello Boolean");
 //npm run dev
 
 //taglio via i primi 2 indici che sono il path
-const boh = process.argv.slice(2);
-console.log(`Hello ${boh}`);
+const parolaPassata = process.argv.slice(2);
+console.log(`Hello ${parolaPassata}`);
 // node index.js "parola passata"
